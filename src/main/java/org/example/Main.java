0d014b4d73e23cc9e25175f8a2820bb29d5d2a60
mikel <mikel.ra.2003@gmail.com>
2024-09-12 14:05:33 +0200

    :add Añadir comentarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
-package org.example;
+/**
+ * Clase de Prueba de Proyecto
+ */
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+package org.example;
+//todo: cambiar de descripcion
 public class Main {
+    /**
+     * Procedimiento main trivial
+     * @param args parametros de entrada ninguno.
+     */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.print("Hello and welcome!");
         System.out.print("Hola Pisha");
 
